01042de80ba8482a55040b450c420a01f4c5c3b4
luqin2007 <lq2007lq@hotmail.com>
2024-03-30 09:11:03 +0800

    @Autowired

--- src/main/java/com/example/mybank/service/CustomerRegistrationServiceImpl.java
+++ src/main/java/com/example/mybank/service/CustomerRegistrationServiceImpl.java
@@ -4,14 +4,24 @@ import com.example.mybank.dao.CustomerRegistrationDao;
 import com.example.mybank.domain.CustomerRegistrationDetails;
 import lombok.AllArgsConstructor;
 import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import java.beans.ConstructorProperties;
 
-@AllArgsConstructor(onConstructor_ = @ConstructorProperties({"customerRegistrationDetails", "customerRegistrationDao"}))
+@Component
 public class CustomerRegistrationServiceImpl implements CustomerRegistrationService {
 
     private final CustomerRegistrationDetails customerRegistrationDetails;
     private final CustomerRegistrationDao customerRegistrationDao;
+
+    @Autowired
+    @ConstructorProperties({"customerRegistrationDetails", "customerRegistrationDao"})
+    public CustomerRegistrationServiceImpl(CustomerRegistrationDetails customerRegistrationDetails, CustomerRegistrationDao customerRegistrationDao) {
+        this.customerRegistrationDetails = customerRegistrationDetails;
+        this.customerRegistrationDao = customerRegistrationDao;
+        System.out.println("CustomerRegistrationService created");
+    }
 
     @Override
     public void setAccountNumber(String accountNumber) {
