f1cd46a482a7257d38ffa6a017384510cabe2f8a
luqin2007 <lq2007lq@hotmail.com>
2024-04-09 01:48:11 +0800

    cache （注解）

--- src/main/java/com/example/mybank/config/CacheConfig.java
+++ src/main/java/com/example/mybank/config/CacheConfig.java
@@ -1,9 +1,22 @@
 package com.example.mybank.config;
 
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.concurrent.ConcurrentMapCache;
+import org.springframework.cache.support.SimpleCacheManager;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.ImportResource;
+
+import java.util.Set;
 
 @Configuration
-@ImportResource("classpath:cache.xml")
+@EnableCaching
 public class CacheConfig {
+
+    @Bean
+    public SimpleCacheManager myCacheManager() {
+        SimpleCacheManager cacheManager = new SimpleCacheManager();
+        cacheManager.setCaches(
+                Set.of(new ConcurrentMapCache("fixedDepositList"), new ConcurrentMapCache("fixedDeposit")));
+        return cacheManager;
+    }
 }
